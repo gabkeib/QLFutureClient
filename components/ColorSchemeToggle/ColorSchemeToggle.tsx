42c71f75e3b63b0a471cdbc59d800b1cebb5469d
Pijus <pijus.petkevicius@bentley.com>
2024-04-13 17:40:11 +0200

    added calculate subgenomes backend request

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -10,6 +10,29 @@ export function ColorSchemeToggle() {
       <Button onClick={() => setColorScheme('light')}>Light</Button>
       <Button onClick={() => setColorScheme('dark')}>Dark</Button>
       <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={() => calculateSubgenomes()}>Click me</Button>
     </Group>
   );
+}
+
+function calculateSubgenomes() {
+  let message = {
+    genome: "string",
+    subgenome: "string"
+  }
+  let stringifiedMessage = JSON.stringify(message)
+  console.log(stringifiedMessage)
+  fetch('https://qlfuturebackend.onrender.com/genome',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: stringifiedMessage
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
 }
