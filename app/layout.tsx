029f05dbbd0553947ac6b2e97aebd7ebc3fbef2d
Gabrielius Keibas <gabkeib@gmail.com>
2024-04-14 10:38:11 +0200

    Change to dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: any }) {
         />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider forceColorScheme="dark">{children}</MantineProvider>
       </body>
     </html>
   );
